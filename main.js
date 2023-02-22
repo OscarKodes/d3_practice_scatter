64d05ca78f1afbf683dceefb4d1dc3c5295bff6e
Oscar Ko <OscarKoEmail@gmail.com>
2023-02-22 14:26:44 -0500

    Change all colors to grey

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 /* CONSTANTS AND GLOBALS */
-const height = window.innerHeight * 0.8;
-const width = window.innerWidth * 0.8;
+const height = window.innerHeight * 0.7;
+const width = window.innerWidth * 0.9;
 
 const margin = {
     top: 50,
     left: 80,
-    right: 50,
+    right: 200,
     bottom: 50
   };
 
@@ -35,15 +35,15 @@ d3.csv("cleaned_data.csv", d3.autoType).then(data => {
 
   // Placing brands and colors in variables to be reused in colorScale & Legend
   const allBrands = Array.from(new Set(data.map(d => d.brand)));
-  const allColors = d3.schemeCategory10;
+  const allColors = new Array(28).fill("grey");
 
   const colorScale = d3.scaleOrdinal()
     .domain(allBrands)
     .range(allColors)
 
-  // const sizeScale = d3.scaleSqrt()
-  //   .domain([1, d3.max(data, d => d["Budget (million $)"])])
-  //   .range([3, 16]);
+  const sizeScale = d3.scaleSqrt()
+    .domain([1, d3.max(data, d => d["Budget (million $)"])])
+    .range([3, 16]);
 
 
 //   /* HTML ELEMENTS ############################################## */
@@ -53,7 +53,7 @@ d3.csv("cleaned_data.csv", d3.autoType).then(data => {
     .append("svg")
     .attr("width", width)
     .attr("height", height)
-    // .style("background-color", "lavender")
+    .style("background-color", "lavender")
 
   // AXIS TICKS  ----------------------------------------------
   svg.append("g")
@@ -82,9 +82,44 @@ d3.csv("cleaned_data.csv", d3.autoType).then(data => {
     .attr("transform", "rotate(-90)")
     .text("Price (USD)");
 
+
+
+  // LEGEND ------------------------------------------------
+
+  // Title for Legend
+  svg.append("text")
+    .text("Brands:")
+    .attr("x", width - margin.right + 70)
+    .attr("y", 60)
+    .style("font-size", "1rem")
+    .style("font-weight", "bold")
+
+  // Color dots for Legend
+  svg.selectAll(".legend-dot")
+    .data(allColors)
+    .join("circle")
+    .attr("class", "legend-dot")
+    .attr("cx", width - margin.right + 60)
+    .attr("cy", (_, i) => 80 + i * 20)
+    .attr("r", 6)
+    .style("fill", d => d)
+    .attr("stroke", "black")
+    .attr("opacity", "0.6")
+
+  // labels for Legend
+  svg.selectAll(".legend-brand")
+    .data(allBrands)
+    .join("text")
+    .attr("class", "legend-brand")
+    .attr("x", width - margin.right + 72)
+    .attr("y", (_, i) => 81 + i * 20)
+    .text(d => d[0].toUpperCase() + d.slice(1))
+    .style("font-size", "15px")
+    .attr("alignment-baseline","middle")
+
+
+
   // DOTS FOR SCATTERPLOT ----------------------------------
-
-
 
   const dot = svg
     .selectAll(".dot") 
@@ -95,5 +130,5 @@ d3.csv("cleaned_data.csv", d3.autoType).then(data => {
     .attr("r", 5)
     .attr("fill", d => colorScale(d.brand))
     .attr("stroke", "black")
-    .attr("opacity", "0.4");
+    .attr("opacity", "0.5");
 });
